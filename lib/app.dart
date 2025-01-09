0bba8e829445d02368050da16bd76c1677b46c8b
jeremienguyen20798 <phuchoang20798@gmail.com>
2025-01-09 08:32:51 +0700

    commit code: Fix bug not ring alarm with production env.

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:smart_clock/core/constants/app_constants.dart';
-//import 'package:smart_clock/features/home/view/home_page.dart';
-import 'package:smart_clock/features/settings/view/settings_page.dart';
+import 'package:smart_clock/features/home/view/home_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -17,6 +16,6 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
         ),
         builder: EasyLoading.init(),
-        home: const SettingsPage());
+        home: const HomePage());
   }
 }
